2b9e47a28a6778f189ce9df77d08936f5fd89726
lordfelipe@gmail.com <lordfelipe@gmail.com@e54d1a93-a3eb-e898-6422-06f40bd02cda>
2011-12-13 11:44:39 +0000

    Vulpe License update to GPL v2

--- trunk/vulpe-framework/controller/base/src/main/java/org/vulpe/controller/validator/EntityValidator.java
+++ trunk/vulpe-framework/controller/base/src/main/java/org/vulpe/controller/validator/EntityValidator.java
@@ -1,17 +1,39 @@
 /**
- * Vulpe Framework - Copyright (c) Active Thread
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Vulpe Framework - Quick and Smart ;)
+ * Copyright (C) 2011 Active Thread
+ * 
+ * Este programa é software livre; você pode redistribuí-lo e/ou
+ * modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+ * publicada pela Free Software Foundation; tanto a versão 2 da
+ * Licença como (a seu critério) qualquer versão mais nova.
+ * 
+ * Este programa é distribuído na expectativa de ser útil, mas SEM
+ * QUALQUER GARANTIA; sem mesmo a garantia implícita de
+ * COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
+ * PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
+ * detalhes.
+ * 
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ * junto com este programa; se não, escreva para a Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+/**
+ * Vulpe Framework - Quick and Smart ;)
+ * Copyright (C) 2011 Active Thread
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package org.vulpe.controller.validator;
 
--- trunk/vulpe-framework/model/base/src/main/java/org/vulpe/model/entity/Parameter.java
+++ trunk/vulpe-framework/model/base/src/main/java/org/vulpe/model/entity/Parameter.java
@@ -1,17 +1,39 @@
 /**
- * Vulpe Framework - Copyright (c) Active Thread
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Vulpe Framework - Quick and Smart ;)
+ * Copyright (C) 2011 Active Thread
+ * 
+ * Este programa é software livre; você pode redistribuí-lo e/ou
+ * modificá-lo sob os termos da Licença Pública Geral GNU, conforme
+ * publicada pela Free Software Foundation; tanto a versão 2 da
+ * Licença como (a seu critério) qualquer versão mais nova.
+ * 
+ * Este programa é distribuído na expectativa de ser útil, mas SEM
+ * QUALQUER GARANTIA; sem mesmo a garantia implícita de
+ * COMERCIALIZAÇÃO ou de ADEQUAÇÃO A QUALQUER PROPÓSITO EM
+ * PARTICULAR. Consulte a Licença Pública Geral GNU para obter mais
+ * detalhes.
+ * 
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ * junto com este programa; se não, escreva para a Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+/**
+ * Vulpe Framework - Quick and Smart ;)
+ * Copyright (C) 2011 Active Thread
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package org.vulpe.model.entity;
 
@@ -50,7 +72,7 @@ public class Parameter {
 	 * @param value
 	 *            Valor do Parametro (no caso de um Array, deve-se deixar nulo)
 	 * @param out
-	 *            Indica se o parametro tamb�m � de saida (in/out)
+	 *            Indica se o parametro tamb�m � de saida (in/out)
 	 */
 	public Parameter(final int type, final Object value, final boolean out) {
 		this.type = type;
@@ -85,7 +107,7 @@ public class Parameter {
 	 * @param value
 	 *            Valor do Parametro (no caso de um Array, deve-se deixar nulo)
 	 * @param out
-	 *            Indica se o parametro tamb�m � de saida (in/out)
+	 *            Indica se o parametro tamb�m � de saida (in/out)
 	 * @param arrayType
 	 *            Tipo do Array no banco - Exemplo: T_LISTA_NUMEROS
 	 * @param arrayValues
